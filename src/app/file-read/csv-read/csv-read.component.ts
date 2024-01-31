c1cb0df3d6da0f6445f87ea6a0c6b542466ff635
David Barón <baronavilandavid@gmail.com>
2024-01-31 12:23:31 -0500

    se hace la funcionalidad visual del drag & drop csv

--- src/app/file-read/csv-read/csv-read.component.ts
+++ src/app/file-read/csv-read/csv-read.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-csv-read',
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class CsvReadComponent {
 
+  @ViewChild('dropZone') dropZoneElement!: ElementRef
+
+  onDrop(event: DragEvent){
+    event.preventDefault()
+
+    const file = event.dataTransfer?.files[0];
+
+    if(file){
+      if(this.isTypeCSV(file)){
+        // codigo a implementar de leída de pene
+      } else{
+        alert('Tipo de archivo no permitido') // implementar con sweet alert 2
+      }
+    }
+  }
+
+  onDragOver(event: DragEvent){
+    event.preventDefault()
+    this.dropZoneElement.nativeElement.style.border = '2px solid #9DBCCD';
+  }
+
+  onDragLeave(event: DragEvent){
+    event.preventDefault()
+    this.dropZoneElement.nativeElement.style.border = '2px dashed #9DBCCD';
+  }
+
+  isTypeCSV(file: File){
+    return file.type === 'text/csv' || file.name.toLowerCase().endsWith('.csv');
+  }
 }
--- src/app/file-read/file-read.module.ts
+++ src/app/file-read/file-read.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FileReadRoutingModule } from './file-read-routing.module';
@@ -12,6 +12,9 @@ import { CsvReadComponent } from './csv-read/csv-read.component';
   imports: [
     CommonModule,
     FileReadRoutingModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class FileReadModule { }
