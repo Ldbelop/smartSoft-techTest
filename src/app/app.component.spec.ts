31faaaa09a8aeb404c5e23e29122614b4fbbb949
David Barón <baronavilandavid@gmail.com>
2024-01-31 09:06:15 -0500

    Se escriben los tests de login (auth/login)
    Tambien se evita que fallen los tests en el componente de app, que espera que retorne un titulo que no va a retornar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'smartSoft-TechInterview-ldbelop'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('smartSoft-TechInterview-ldbelop');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, smartSoft-TechInterview-ldbelop');
-  });
 });
--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,16 +9,46 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    })
-    .compileComponents();
-    
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a form with valid initial state', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+    // Add more expectations as needed
+  });
+
+  it('should disable the submit button initially', () => {
+    const submitButton: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    expect(submitButton.disabled).toBeTruthy();
+  });
+
+  it('should enable the submit button when the form is valid', () => {
+    // Simulate filling out the form with valid data
+    component.loginForm.setValue({
+      email: 'test@example.com',
+      pwd: 'password123',
+    });
+    fixture.detectChanges();
+
+    const submitButton: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    expect(submitButton.disabled).toBeFalsy();
+  });
+
+  // Add more tests for the component's behavior and interactions
+
 });
