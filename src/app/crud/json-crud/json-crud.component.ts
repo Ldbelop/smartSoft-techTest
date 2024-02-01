b5b8f6890ee3e653f036e8fe3681c55e4a3a2d8c
David Barón <baronavilandavid@gmail.com>
2024-02-01 16:34:14 -0500

    se implementa hamburguer menu en crud

--- src/app/crud/json-crud/json-crud.component.ts
+++ src/app/crud/json-crud/json-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-json-crud',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './json-crud.component.css'
 })
 export class JsonCrudComponent {
+  showHamburguer: boolean = false;
 
+  constructor(private authService: AuthService){}
+
+  logout(){
+    this.authService.logout()
+  }
 }
