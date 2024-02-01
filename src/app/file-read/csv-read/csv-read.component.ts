ec0829f7be32b70dac143422ad6aadb7b9169cb6
David Barón <baronavilandavid@gmail.com>
2024-01-31 22:09:06 -0500

    Se hace parseo de datos de CSV con papaParser

--- src/app/file-read/csv-read/csv-read.component.ts
+++ src/app/file-read/csv-read/csv-read.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as Papa from 'papaparse'
 
 @Component({
   selector: 'app-csv-read',
@@ -12,15 +13,39 @@ export class CsvReadComponent {
   onDrop(event: DragEvent){
     event.preventDefault()
 
-    const file = event.dataTransfer?.files[0];
+    const file:any = event.dataTransfer?.files[0];
 
     if(file){
       if(this.isTypeCSV(file)){
-        // codigo a implementar de leída de pene
+        this.parseCSVFile(file)
       } else{
         alert('Tipo de archivo no permitido') // implementar con sweet alert 2
       }
     }
+  }
+
+  parseCSVFile(file: File): void {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      if (event.target) {
+        const csvString = event.target.result as string;
+        Papa.parse(csvString, {
+          complete: (results) => {
+            console.log('Parsed CSV data:', results.data);
+          },
+          error: (error: any) => {
+            console.error('Error parsing CSV:', error.message);
+          },
+        });
+      }
+    };
+
+    reader.onerror = (error) => {
+      console.error('Error reading file:', error);
+    };
+
+    reader.readAsText(file);
   }
 
   onDragOver(event: DragEvent){
